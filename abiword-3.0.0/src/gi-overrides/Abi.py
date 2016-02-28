6b1fba07e5ad3bdd2d603c90a0ef081ce1bf6f84
Moad Ben-Suleiman <moadben95@gmail.com>
2016-02-28 12:31:19 -0800

    packet sniffer added

--- abiword-3.0.0/src/gi-overrides/Abi.py
+++
@@ -1,9 +0,0 @@
-import sys
-from gi.repository import Gtk
-from ..overrides import override
-from ..importer import modules
-
-Abi = modules['Abi']._introspection_module
-
-# Initialize libabiword
-Abi.init(sys.argv)
